e28732adcfd056f58d8ec7d483e39628fe4441f3
johnoutlier <jvera@outlier.digital>
2019-07-19 17:57:42 +0800

    fixing pagination

--- resources/assets/js/components/VideoListComponent.vue
+++ resources/assets/js/components/VideoListComponent.vue
@@ -3,8 +3,6 @@
       <div class="col-md-1">
       </div>
       <div class="col-md-12">
-
-        
           <h3>Videos</h3>
             <div class="col-12">
 
@@ -13,33 +11,41 @@
                         <input type="text" class="form-control" placeholder="Search Video" v-model="search">
                         <div class="col-4" v-for="vid in list.data" id="video-list">
                             <img class="card-img" :src="'https://img.youtube.com/vi/'+vid.embedlink+'/mqdefault.jpg'">
-                            <a href="vid.slug">
+                            
+                            <a :href="'video/'+vid.slug">
                             <div class="card-img-overlay">
                                 <p class="card-title d-inline-flex">{{vid.title}}</p>
                             </div>
                             </a>
                         </div>
-                     
-                        <nav aria-label="...">
+                    </div>
+                    <nav aria-label="...">
                             <ul class="pagination">
-                                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                               
-                                    <li class="page-item" v-for="count in pageNumbers">
-                                        <a class="page-link" href="#" >
-                                            {{count.counter}}
+                                <li class="page-item">
+                                    <a class="page-link" href="#" aria-label="Previous" @click="getAll(list.current_page-1)">
+                                        <span aria-hidden="true">&laquo;</span>
+                                        <span class="sr-only">Previous</span>
+                                    </a>
+                                </li>                               
+                                  
+                                    <li class="page-item" v-for="num in pageNumbers">
+                                        <a class="page-link" href="#" @click="getAll(num.counter)" id="pageNumsLink">
+                                            {{num.counter}}
                                         </a>
                                     </li>       
                                 
-                                <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                                <li class="page-item">
+                                    <a class="page-link" href="#" aria-label="Next" @click="getAll(list.current_page+1)">
+                                        <span aria-hidden="true">&raquo;</span>
+                                        <span class="sr-only">Next</span>
+                                    </a>
+                                </li>
                             </ul>
                         </nav>
-                        {{this.list.last_page}}
-                    </div>
                 </div>  
 
             </div>
             <hr>
-
       </div>
      
   </div>
@@ -60,6 +66,9 @@
 
                 perPage:9,
                 currentPage:1,
+
+                lastPage:0,
+                ajaxLoad: 0
             };
         },
 
@@ -73,46 +82,67 @@
             }
         },
         methods:{           
-            getAll(endpoint){                
+            getAll(page){
+                var output = [];
+                var pageUrl = this.endpoint+"?page="+page;
                 if(this.search){
-                    this.pageUrl = this.endpoint+"?search="+this.search;
+                    pageUrl = pageUrl+"&search="+this.search;
+
                 }
-                else{
-                    this.pageUrl = this.endpoint;
-                }
-                axios.get(this.pageUrl).then(response=>this.list = response.data);
+               
+                axios.get(pageUrl)
+                    .then(response=> {this.list = response.data; 
+                                        this.lastPage = response.data.last_page;
+                                        if(this.ajaxLoad == 0 ){
+                                            this.pages();
+                                        }
+                                        this.ajaxLoad = 1;
+                                    }).catch(err => console.error(err));
             },
 
             pages(){
-                this.pageNumbers = [];
-                var count = 0;
-                for(var i = 1; i <= this.list.last_page; i++){
-                    count +=1;
+                var count = 1;
+                var lastPage = this.lastPage
+
+                for(var i = 1; i <= lastPage; i++){
                     this.pageNumbers.push({
-                        isActive: "", 
-                        counter: count
-                        });                    
+                        isActive: false,  
+                        counter: i
+                        });
+
+                    if(this.list.currnetPage == count){
+                        this.pageNumbers.push({
+                            isActive: true, 
+                            counter: i
+                        });
+                    }
+
+                    if(count == 7){
+                        this.pageNumbers.push({
+                            isActive: true, 
+                            counter: '...'
+                        });
+                        count = 1
+                    }
+                count +=1;                        
                 }
-
-                    console.log(this.list.last_page);
-
-            }
-        },
-
-        mounted(){
-            this.pages();
+            },
         },
 
         created(){
-            this.getAll();
+            this.getAll(1);
+            // this.pages();
+        },
+
+        mounted(){
+            // this.pages();
+            // console.log(this.lastPage);
         },
 
         watch:{
             search(){
-                this.getAll(this.endpoint);
+                this.getAll(1);
                 this.pageNum = 1;
-                this.pages();
-
             }
         },
     }
